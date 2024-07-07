ea4d373a2136b6c9401c4715ba271bdfedf3b1d0
Intan Amelia <84213559+IntanAmelia@users.noreply.github.com>
2024-07-07 21:26:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -56,6 +56,8 @@ elif menu == "Imputasi Missing Value Menggunakan KNN":
         st.write(missing_data)
         df_imputed = pd.read_csv('imputasi_fix_n_4.csv')
         st.session_state.df_imputed = df_imputed
+        df = df.reset_index(drop=True)
+        df_imputed = df_imputed.reset_index(drop=True)
         df_comparison = pd.concat([df, df_imputed], axis=1)
         st.write('Data yang telah dilakukan Proses Imputasi Missing Value dengan KNN')
         st.write(df_comparison)
