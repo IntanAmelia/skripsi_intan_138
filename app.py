047cc60de6f1020866c8779a80e2a04c07fd6df2
Intan Amelia <84213559+IntanAmelia@users.noreply.github.com>
2024-07-08 06:21:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -132,7 +132,7 @@ elif menu == "Model LSTM":
     else:
         st.write('Silahkan melakukan proses normalisasi data terlebih dahulu.')
 elif menu == "Prediksi LSTM":
-    if st.session_state.df_imputed is not None and st.session_state.x_train is not None and st.session_state.x_test is not None and st.session_state.y_train is not None and st.session_state.y_test is not None and st.session_state.model is not None and st.session_state.scaler is not None and st.session_state.scaled_data is not None and st.session_state.training_data_len is not None and st.session_state.time_steps is not None:
+    if st.session_state.df_imputed is not None and st.session_state.model is not None and st.session_state.scaler is not None and st.session_state.scaled_data is not None:
         test_predictions = st.session_state.model.predict(st.session_state.x_test)
         test_predictions_data = st.session_state.scaler.inverse_transform(test_predictions)
         data_prediksi_uji = pd.DataFrame(test_predictions_data, columns=['Hasil Prediksi Data Uji'])
